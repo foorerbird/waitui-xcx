1c94fdd8133b1694db5b5805875c720e245c60f2
footerbird <footerbird@163.com>
2018-06-18 09:56:40 +0800

    完善资讯模块

---
+++ pages/article/detail.js
@@ -0,0 +1,45 @@
+//detail.js
+//获取应用实例
+const app = getApp()
+var WxParse = require('../../wxParse/wxParse.js');
+
+Page({
+  data: {
+    news_title: '',
+    figure_path: '',
+    author_name: '',
+    create_time: '',
+    news_lead: '',
+    news_content: ''
+  },
+  onLoad: function (option) {
+    var that = this;
+    that.getArticleDetail(option.news_id);
+  },
+  getArticleDetail: function(news_id) {
+    var that = this;
+    wx.request({
+      url: 'http://140.143.5.238/Index_controller/get_articleDetailAjax?news_id='+news_id, //请求外推网资讯详情接口
+      success: function(res) {
+        that.setData({
+          news_title: res.data.article.news_title
+        });
+        that.setData({
+          figure_path: res.data.article.figure_path
+        });
+        that.setData({
+          author_name: res.data.article.author_name
+        });
+        that.setData({
+          create_time: res.data.article.create_time
+        });
+        that.setData({
+          news_lead: res.data.article.news_lead
+        });
+        var news_content = res.data.article.news_content;
+        news_content = news_content.replace(/\\=""/g,'');
+        WxParse.wxParse('article', 'html', news_content, that);
+      }
+    })
+  }
+})
--- pages/article/index.js
+++ pages/article/index.js
@@ -4,51 +4,61 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello 头条',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    article_list: [],
+    article_page: 1,//10条一页，每次加载10条
+    loading: true
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    var that = this;
+    that.getArticleList();
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  onPullDownRefresh: function(){
+    var that = this;
+    that.setData({
+      article_page: 1
+    })
+    that.refreshArticleList();
+  },
+  onReachBottom: function(){
+    var that = this;
+    if(!that.data.loading){
+        return;
+    }
+    that.getArticleList();
+  },
+  getArticleList: function(e) {
+    var that = this;
+    wx.request({
+      url: 'http://140.143.5.238/Index_controller/get_articleAjax?page='+that.data.article_page, //请求外推网资讯列表接口
+      success: function(res) {
+        console.log(res.data.article_list)
+        that.setData({
+          article_list: that.data.article_list.concat(res.data.article_list)
+        });
+        that.setData({
+          article_page: that.data.article_page+1
+        });
+        if(res.data.article_list.length < 10){
+          that.setData({
+            loading: false
+          });
+        }
+      }
+    })
+  },
+  refreshArticleList: function(e) {
+    var that = this;
+    wx.request({
+      url: 'http://140.143.5.238/Index_controller/get_articleAjax?page='+that.data.article_page, //请求外推网资讯列表接口
+      success: function(res) {
+        that.setData({
+          article_list: res.data.article_list
+        });
+        that.setData({
+          article_page: 1
+        });
+        wx.stopPullDownRefresh()
+      }
     })
   }
 })
