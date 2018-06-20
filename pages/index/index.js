f24043b822a5ddd2ff28e5923bcb9f751b49b7bd
footerbird <footerbird@163.com>
2018-06-21 00:06:36 +0800

    添加首页广告swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,25 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello 首页',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    ad_list: []
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    var that = this;
+    that.getAdvertiseList(10);
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  getAdvertiseList: function(limit) {
+    var that = this;
+    wx.request({
+      url: 'http://140.143.5.238/Index_controller/get_advertisementAjaxImage', //请求外推网广告列表接口
+      data: {
+        limit: limit
+      },
+      success: function(res) {
+        console.log(res.data.ad_list)
+        that.setData({
+          ad_list: that.data.ad_list.concat(res.data.ad_list)
+        });
+      }
     })
   }
 })
