a87a1f497ee7f377ec55c423c3f522b637466551
footerbird <footerbird@163.com>
2018-07-15 10:48:07 +0800

    文章接口修改调整

--- pages/article/detail.js
+++ pages/article/detail.js
@@ -5,24 +5,24 @@ var WxParse = require('../../wxParse/wxParse.js');
 
 Page({
   data: {
-    news_title: '',
+    article_title: '',
     figure_path: '',
     author_name: '',
     create_time: '',
-    news_lead: '',
-    news_content: ''
+    article_lead: '',
+    article_content: ''
   },
   onLoad: function (option) {
     var that = this;
-    that.getArticleDetail(option.news_id);
+    that.getArticleDetail(option.article_id);
   },
-  getArticleDetail: function(news_id) {
+  getArticleDetail: function(article_id) {
     var that = this;
     wx.request({
-      url: 'http://140.143.5.238/mobile/Index_controller/get_articleDetailAjax?news_id='+news_id, //请求外推网资讯详情接口
+      url: 'http://140.143.5.238/mobile/Index_controller/get_articleDetailAjax?article_id='+article_id, //请求外推网资讯详情接口
       success: function(res) {
         that.setData({
-          news_title: res.data.article.news_title
+          article_title: res.data.article.article_title
         });
         that.setData({
           figure_path: res.data.article.figure_path
@@ -34,11 +34,11 @@ Page({
           create_time: res.data.article.create_time
         });
         that.setData({
-          news_lead: res.data.article.news_lead
+          article_lead: res.data.article.article_lead
         });
-        var news_content = res.data.article.news_content;
-        news_content = news_content.replace(/\\=""/g,'');
-        WxParse.wxParse('article', 'html', news_content, that);
+        var article_content = res.data.article.article_content;
+        article_content = article_content.replace(/\\=""/g,'');
+        WxParse.wxParse('article', 'html', article_content, that);
       }
     })
   }
